69e112e7ab588bc86f7e0c00fa8246cd4981ecb3
mlavin <markdlavin@gmail.com>
2012-01-07 12:29:49 -0500

    Initial setup.py.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+import pymunin
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(pip 
+    name='PyMunin',
+    version=pymunin.__version__,
+    author=pymunin.__author__,
+    author_email=pymunin.__email__,
+    packages=find_packages(),
+    include_package_data=True,
+    url='http://aouyar.github.com/PyMunin',
+    license=pymunin.__license__,
+    description=u'Python Module for developing Munin Multigraph Monitoring Plugins.',
+    classifiers=[
+        'Topic :: System :: Monitoring',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.markdown'),
+)
