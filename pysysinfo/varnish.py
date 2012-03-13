e4c8f920b9d8d257a588370165f7a20037260806
Ali Onur Uyar <aouyar@gmail.com>
2012-03-13 12:42:45 -0600

    Permit querying other instances of Varnish.

--- pysysinfo/varnish.py
+++ pysysinfo/varnish.py
@@ -5,7 +5,6 @@ The statistics are obtained by running the command varnishstats.
 """
 
 import re
-import commands
 import util
 
 __author__ = "Ali Onur Uyar"
@@ -19,7 +18,7 @@ __status__ = "Development"
 
 
 # Defaults
-varnishstatCmd = "varnishstat -1"
+varnishstatCmd = "varnishstat"
 
 
 
@@ -27,6 +26,15 @@ class VarnishInfo:
     """Class to retrieve stats from Varnish Proxy Server."""
     
     _descDict = {}
+    
+    def __init__(self, instance=None):
+        """Initialization for monitoring Varnish Proxy Server Instance.
+        
+        @param instance: Name  of the Varnish Proxy Server instance.
+                        (Defaults to hostname.)
+        """
+        self._instance = instance
+        
 
     def getStats(self):
         """Runs varnishstats command to get stats from Varnish Proxy Server.
@@ -35,19 +43,19 @@ class VarnishInfo:
 
         """
         info_dict = {}
-        (retval, output) = commands.getstatusoutput(varnishstatCmd)
-        if retval == 0:
-            if self._descDict is None:
-                self._descDict = {}
-            for line in output.splitlines():
-                mobj = re.match('(\S+)\s+(\d+)\s+(\d+\.\d+|\.)\s+(\S.*\S)\s*$', 
-                                line)
-                if mobj:
-                    info_dict[mobj.group(1)] = util.parse_value(mobj.group(2))
-                    self._descDict[mobj.group(1)] = mobj.group(4)
-            return info_dict
-        else:
-            raise Exception("Execution of command failed: %s" % varnishstatCmd)
+        args = [varnishstatCmd, '-1']
+        if self._instance is not None:
+            args.extend(['-n', self._instance])
+        output = util.exec_command(args)
+        if self._descDict is None:
+            self._descDict = {}
+        for line in output.splitlines():
+            mobj = re.match('(\S+)\s+(\d+)\s+(\d+\.\d+|\.)\s+(\S.*\S)\s*$', 
+                            line)
+            if mobj:
+                info_dict[mobj.group(1)] = util.parse_value(mobj.group(2))
+                self._descDict[mobj.group(1)] = mobj.group(4)
+        return info_dict
         return info_dict
     
     def getDescDict(self):
