a890a3727c735b89bf5851f2be2f36ddbfcdecf5
Preston Mason <pentie@gmail.com>
2013-01-09 18:29:16 +0800

    add fragment to phpapcstat
    Conflicts in ext/apcinfo.php resolved manually. Changes
    to pymunin/plugins/phpapcstats.py were reverted to preserve
    original graphs.

--- ext/apcinfo.php
+++ ext/apcinfo.php
@@ -19,7 +19,8 @@ header('Cache-Control: max-age=0, no-cache, '
 
 $cache_sys = apc_cache_info('', true);
 $cache_user = apc_cache_info('user', true);  
-$memory = apc_sma_info(true);
+$mem_limited = apc_sma_info(true);
+$mem = apc_sma_info();
 
 foreach ($cache_sys as $key => $val) {
   printf("%s:%s:%s\n",'cache_sys', $key, $val); 
@@ -27,8 +28,30 @@ foreach ($cache_sys as $key => $val) {
 foreach ($cache_user as $key => $val) {
   printf("%s:%s:%s\n",'cache_user', $key, $val); 
 }
-foreach ($memory as $key => $val) {
+foreach ($mem_limited as $key => $val) {
   printf("%s:%s:%s\n",'memory', $key, $val); 
 }
+
+// Fragementation: (freeseg - 1) / total_seg
+$nseg = $freeseg = $fragsize = $freetotal = 0;
+for($i=0; $i<$mem['num_seg']; $i++) {
+        $ptr = 0;
+        foreach($mem['block_lists'][$i] as $block) {
+                if ($block['offset'] != $ptr) {
+                        ++$nseg;
+                }
+                $ptr = $block['offset'] + $block['size'];
+                /* Only consider blocks <5M for the fragmentation % */
+                if($block['size']<(5*1024*1024)) $fragsize+=$block['size'];
+                $freetotal+=$block['size'];
+        }
+        $freeseg += count($mem['block_lists'][$i]);
+}
+
+$frag = $freeseg > 1 ? $fragsize/$freetotal : 0;
+
+printf("%s:%s:%s\n",'memory', 'fragmented', $fragsize); 
+printf("%s:%s:%f\n",'memory', 'fragment_percentage', $frag * 100); 
+
 
 ?>
